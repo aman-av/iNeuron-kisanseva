b2b4e32ac4f6eb37b03d9e5277c27b1fba4c3c8d
aman verma <aman9890.av@example.com>
2022-07-02 20:36:39 +0530

    redeploy5

--- app.js
+++ app.js
@@ -78,15 +78,15 @@ try{
 	// socket io is required for successful connection between peers
 	// const io = require('socket.io')(server)
 	
-const mongoose = require("mongoose"); // mongo DB used to save users's account info
+// const mongoose = require("mongoose"); // mongo DB used to save users's account info
 // const cors = require("cors");
-const passport = require("passport"); // passport-local used for user authentication
+// const passport = require("passport"); // passport-local used for user authentication
 // const passportLocal = require("passport-local").Strategy;
 // const cookieParser = require("cookie-parser");
-const session = require("express-session");
+// const session = require("express-session");
 // const bodyParser = require("body-parser");
 // const flash = require("express-flash");
-const config = require('./config/key')
+// const config = require('./config/key')
 
 
 // const dbURI = config.mongoURI
@@ -111,20 +111,20 @@ const config = require('./config/key')
 		//   	})
 		// );
 		
-		app.use(
-			session({
-				secret: "secretcode",
-				resave: true,
-				saveUninitialized: true,
-			})
-			);
+		// app.use(
+		// 	session({
+		// 		secret: "secretcode",
+		// 		resave: true,
+		// 		saveUninitialized: true,
+		// 	})
+		// 	);
 			
-			// app.use(cookieParser("secretcode"));
+		// 	// app.use(cookieParser("secretcode"));
 			
-			////Initializing local-passport for user authentication
-			app.use(passport.initialize());
-			app.use(passport.session());
-			require("./middleware/passport-config")(passport);
+		// 	////Initializing local-passport for user authentication
+		// 	app.use(passport.initialize());
+		// 	app.use(passport.session());
+		// 	require("./middleware/passport-config")(passport);
 			
 			
 			
